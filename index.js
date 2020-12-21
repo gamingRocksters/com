d7a445232cfc9bde14947aae1992bfd5cacf2d37
gamingRocksters <71007906+gamingRocksters@users.noreply.github.com>
2020-12-21 07:39:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var $messages = $('.messages-content'),
+var messages = ('.messages-content'),
     d, h, m,
     i = 0;
 
 var myName = "";
 
-$(window).load(function() {
+(window).load(function() {
   myName = prompt("Enter your name");
-  $messages.mCustomScrollbar();
+  messages.mCustomScrollbar();
 
   firebase.database().ref("messages").on("child_added", function (snapshot) {
     if (snapshot.val().sender == myName) {
-      $('<div class="message message-personal"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().message + '<button class="btn-delete" data-id="' + snapshot.key + '" onclick="deleteMessage(this);">Delete</button></div></div>').appendTo($('.mCSB_container')).addClass('new');
-      $('.message-input').val(null);
+      ('<div class="message message-personal"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().message + '<button class="btn-delete" data-id="' + snapshot.key + '" onclick="deleteMessage(this);">Delete</button></div></div>').appendTo(('.mCSB_container')).addClass('new');
+      ('.message-input').val(null);
     } else {
-      $('<div class="message new"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().sender + ': ' + snapshot.val().message + '</div></div>').appendTo($('.mCSB_container')).addClass('new');
+      ('<div class="message new"><figure class="avatar"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpdX6tPX96Zk00S47LcCYAdoFK8INeCElPeJrVDrh8phAGqUZP_g" /></figure><div id="message-' + snapshot.key + '">' + snapshot.val().sender + ': ' + snapshot.val().message + '</div></div>').appendTo(('.mCSB_container')).addClass('new');
     }
     
     setDate();
@@ -23,7 +23,7 @@ $(window).load(function() {
 });
 
 function updateScrollbar() {
-  $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
+  messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
     scrollInertia: 10,
     timeout: 0
   });
@@ -33,24 +33,24 @@ function setDate(){
   d = new Date()
   if (m != d.getMinutes()) {
     m = d.getMinutes();
-    $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    ('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo(('.message:last'));
   }
 }
 
 function insertMessage() {
-  msg = $('.message-input').val();
-  if ($.trim(msg) == '') {
+  msg = ('.message-input').val();
+  if (trim(msg) == '') {
     return false;
   }
 
   sendMessage();
 }
 
-$('.message-submit').click(function() {
+('.message-submit').click(function() {
   insertMessage();
 });
 
-$(window).on('keydown', function(e) {
+(window).on('keydown', function(e) {
   if (e.which == 13) {
     insertMessage();
     return false;
